bb71cb4df42d3083faf3a087379b79655d95e055
vickytc-portfolio <58129130+vickytc-portfolio@users.noreply.github.com>
2023-01-24 06:30:01 -0500

    Update home.py

--- home.py
+++ home.py
@@ -48,7 +48,7 @@ left_column,right_column = st.columns(2)
 
 with left_column:
     st.subheader("Hi, I am Vicky :wave:")
-    st.header(":red[Passionate for understanding users' needs and data!]")
+    st.header(":red[Passionate for data and finding users' needs!]")
     st.write("I enjoy finding ways to automate stuff and interpret data efficiently.")
     st.write("**[My links >](https://linktr.ee/vickytc)**")
 
